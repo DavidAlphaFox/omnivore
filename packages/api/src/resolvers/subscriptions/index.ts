f57b5c2ab359b2ac1e7bce9122d0586ac3be6eb8
Hongbo Wu <hongbo@omnivore.app>
2023-09-14 12:22:27 +0900

    fix rebase conflicts

--- packages/api/src/resolvers/subscriptions/index.ts
+++ packages/api/src/resolvers/subscriptions/index.ts
@@ -1,4 +1,5 @@
 import Parser from 'rss-parser'
+import { Brackets } from 'typeorm'
 import { Subscription } from '../../entity/subscription'
 import { env } from '../../env'
 import {
@@ -28,7 +29,6 @@ import { Merge } from '../../util'
 import { analytics } from '../../utils/analytics'
 import { enqueueRssFeedFetch } from '../../utils/createTask'
 import { authorized } from '../../utils/helpers'
-import { Brackets } from 'typeorm'
 
 type PartialSubscription = Omit<Subscription, 'newsletterEmail'>
 
@@ -45,25 +45,20 @@ export const subscriptionsResolver = authorized<
   SubscriptionsSuccessPartial,
   SubscriptionsError,
   QuerySubscriptionsArgs
->(
-  async (
-    _obj,
-    { sort, type = SubscriptionType.Newsletter }, // default to newsletter
-    { authTrx, uid, log }
-  ) => {
-    try {
-      const sortBy =
-        sort?.by === SortBy.UpdatedTime ? 'lastFetchedAt' : 'createdAt'
-      const sortOrder = sort?.order === SortOrder.Ascending ? 'ASC' : 'DESC'
+>(async (_obj, { sort, type }, { authTrx, uid, log }) => {
+  try {
+    const sortBy =
+      sort?.by === SortBy.UpdatedTime ? 'lastFetchedAt' : 'createdAt'
+    const sortOrder = sort?.order === SortOrder.Ascending ? 'ASC' : 'DESC'
 
-      const subscriptions = await authTrx(async (t) => {
-        const queryBuilder = t
-          .getRepository(Subscription)
-          .createQueryBuilder('subscription')
-          .leftJoinAndSelect('subscription.newsletterEmail', 'newsletterEmail')
-          .where({
-            user: { id: uid },
-          })
+    const subscriptions = await authTrx(async (t) => {
+      const queryBuilder = t
+        .getRepository(Subscription)
+        .createQueryBuilder('subscription')
+        .leftJoinAndSelect('subscription.newsletterEmail', 'newsletterEmail')
+        .where({
+          user: { id: uid },
+        })
 
       if (type && type == SubscriptionType.Newsletter) {
         queryBuilder.andWhere({
@@ -87,10 +82,8 @@ export const subscriptionsResolver = authorized<
         )
       }
 
-        return queryBuilder
-          .orderBy('subscription.' + sortBy, sortOrder)
-          .getMany()
-      })
+      return queryBuilder.orderBy('subscription.' + sortBy, sortOrder).getMany()
+    })
 
     return {
       subscriptions,
