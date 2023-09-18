c6796bb1902fd820a36ff9a82ad5bce13182ad83
Hongbo Wu <hongbo@omnivore.app>
2023-09-18 22:52:54 +0800

    add migration script

--- packages/api/src/schema.ts
+++ packages/api/src/schema.ts
@@ -392,6 +392,7 @@ const schema = gql`
     readAt: Date
     recommendations: [Recommendation!]
     wordsCount: Int
+    note: String
   }
 
   # Query: article
@@ -684,7 +685,6 @@ const schema = gql`
   enum HighlightType {
     HIGHLIGHT
     REDACTION
-    NOTE
   }
 
   # Highlight
@@ -1604,6 +1604,7 @@ const schema = gql`
     wordsCount: Int
     content: String
     archivedAt: Date
+    note: String
   }
 
   type SearchItemEdge {
